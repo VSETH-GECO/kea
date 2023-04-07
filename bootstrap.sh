f27b5d567ff212cd3c70c962486d92e9555ed865
Maximilian Falkenstein <maxf@njsm.de>
2023-04-07 16:00:53 +0200

    Add super duper experimental IPv6 support

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,11 +16,14 @@ export HOSTNAME="$(hostname -f)"
 echo "HOSTNAME: ${HOSTNAME}"
 # KEA_DB_PASSWORD is not printed
 envsubst '$KEA_DB_HOST,$KEA_DB_PORT,$KEA_DB_USER,$KEA_DB_DB,$KEA_DB_PASSWORD,$HOSTNAME' < /etc/kea/kea-dhcp4.conf.env > /etc/kea/kea-dhcp4.conf
+envsubst '$KEA_DB_HOST,$KEA_DB_PORT,$KEA_DB_USER,$KEA_DB_DB,$KEA_DB_PASSWORD,$HOSTNAME' < /etc/kea/kea-dhcp6.conf.env > /etc/kea/kea-dhcp6.conf
 
 echo "Startmode: $1 $2"
 
 if [ "$1" = "agent" ] ; then
   exec /usr/sbin/kea-ctrl-agent -c /etc/kea/kea-ctrl-agent.conf
+elif [ "$1" = "dhcpv6" ]; then
+  exec /usr/sbin/kea-dhcp6 -c /etc/kea/kea-dhcp6.conf
 else
   exec /usr/sbin/kea-dhcp4 -c /etc/kea/kea-dhcp4.conf
 fi
